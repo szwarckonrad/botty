1e22c1aec61eb6622a813c07c03266c2081dc90f
Konrad Szwarc <k.szwarc@rynekpierwotny.pl>
2018-08-15 19:10:29 +0200

    Test Slacks command request.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import * as express from "express";
+import  * as bodyParser from "body-parser";
 
-const app1 = express();
+const app = express();
 
 const port = process.env.PORT || 8080;
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app1.get("/", (req, res) => res.send("Hello World!"));
-app1.listen(port, () => console.log("Listening on port: ", port));
-
-export const app = app1;
+app.post("/", (req, res) => console.log(req.body) || res.send("Hello World!"));
+app.listen(port, () => console.log("Listening on port: ", port));
