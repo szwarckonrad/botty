031058e111bd09335472283b9cecea470f40e40f
Konrad Szwarc <k.szwarc@rynekpierwotny.pl>
2018-08-15 19:59:28 +0200

    Experimenting with Slack's api.

--- src/handlers/testHandler.ts
+++ src/handlers/testHandler.ts
@@ -1,5 +1,6 @@
 import {Request, Response} from "express";
 
 export const testHandler = (req: Request, res: Response) => {
-    res.status(200).send("*Hello* you @Konrad :thumbsup:")
+    console.log(req.body);
+    res.status(200).json({"text": "*Hello* you <@Konrad> :thumbsup:"})
 };
