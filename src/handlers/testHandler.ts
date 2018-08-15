e61c1f598832db6f556c56417cc2a6a20c5a273b
Konrad Szwarc <k.szwarc@rynekpierwotny.pl>
2018-08-15 20:03:17 +0200

    Experimenting with Slack's api 3.

--- src/handlers/testHandler.ts
+++ src/handlers/testHandler.ts
@@ -2,5 +2,5 @@ import {Request, Response} from "express";
 
 export const testHandler = (req: Request, res: Response) => {
     console.log(req.body);
-    res.status(200).json({"text": "*Hello* you <@Konrad> :thumbsup:", "link_names": 1})
+    res.status(200).json({"text": "*Hello* you <@Konrad> :thumbsup:", "link_names": "1"})
 };
