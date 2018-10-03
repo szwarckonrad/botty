ad771239a2105024524cc650b5989cf7b828b876
Konrad Szwarc <k.szwarc@rynekpierwotny.pl>
2018-10-03 16:26:37 +0200

    test Slack queries.

--- src/handlers/deployHandler.ts
+++ src/handlers/deployHandler.ts
@@ -3,6 +3,7 @@ import {ISlackRequest} from "../common/interfaces/ISlackRequest";
 import {parseRequestText} from "../common/helpers/parseRequestText";
 
 export const deployHandler = (req: Request, res: Response) => {
+    console.log(req.body);
     if (!(req.body as ISlackRequest).text) {
         return res.status(404).send({error: "Doesn't seem like a valid query."});
     }
