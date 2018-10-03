a233ee60aff6e66be71a35cbddbc6582c352ef0f
Konrad Szwarc <k.szwarc@rynekpierwotny.pl>
2018-10-03 15:45:07 +0200

    Test with Slack's api.

--- src/common/helpers/response200.ts
+++
@@ -1,5 +0,0 @@
-import {Response} from "express";
-
-export const response200 = (res: Response, text: string) => (
-    res.send(200).json({"text": text})
-);
--- src/handlers/deployHandler.ts
+++ src/handlers/deployHandler.ts
@@ -1,11 +1,13 @@
 import {Request, Response} from "express";
 import {ISlackRequest} from "../common/interfaces/ISlackRequest";
-import {response200} from "../common/helpers/response200";
 import {parseRequestText} from "../common/helpers/parseRequestText";
 
 export const deployHandler = (req: Request, res: Response) => {
-    const parsedRequestText = parseRequestText((req.body as ISlackRequest).text);
-    const text = "";
+    if (!(req.body as ISlackRequest).text) {
+        return res.status(404).send({error: "Doesn't seem like a valid query."});
+    }
 
-    return response200(res, text);
+    const parsedRequestText = parseRequestText((req.body as ISlackRequest).text);
+
+    return res.status(200).send(parsedRequestText);
 };
