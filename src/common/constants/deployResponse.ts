d19fb3b43a8834bc6fb0cc9446de0049c897d326
Konrad Szwarc <k.szwarc@rynekpierwotny.pl>
2018-10-24 14:48:20 +0200

    Missing coma + line breaks.

--- src/common/constants/deployResponse.ts
+++ src/common/constants/deployResponse.ts
@@ -4,5 +4,5 @@ const conditionalState = (state?: string) => state === "start" ? "*has started*"
 
 export const deployResponseText = (responseData: IDeployResponseData) =>
     `<!channel> | <@${responseData.user}> ${conditionalState(responseData.state)} deploy procedure of` +
-    ` \`${responseData.project}@${responseData.branch}\` on ${responseData.stage}.`;
+    ` \`${responseData.project}@${responseData.branch}\` on *${responseData.stage}*.`;
 
