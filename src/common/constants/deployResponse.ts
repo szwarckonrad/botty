62e2e9ecb47b4b025d95ba6916c79ab7479f2d43
Konrad Szwarc <k.szwarc@rynekpierwotny.pl>
2018-10-24 14:46:41 +0200

    Missing coma + line breaks.

--- src/common/constants/deployResponse.ts
+++ src/common/constants/deployResponse.ts
@@ -1,4 +1,8 @@
 import {IDeployResponseData} from "../helpers/createDeployResponse";
 
+const conditionalState = (state?: string) => state === "start" ? "*has started*" : "*finished*";
+
 export const deployResponseText = (responseData: IDeployResponseData) =>
-    `<!channel> | <@${responseData.user}> ${responseData.state === "start" ? "*has started*" : "*finished*"} deploy procedure of \`${responseData.project}@${responseData.branch}\` on ${responseData.stage}`;
+    `<!channel> | <@${responseData.user}> ${conditionalState(responseData.state)} deploy procedure of` +
+    ` \`${responseData.project}@${responseData.branch}\` on ${responseData.stage}.`;
+
--- src/common/constants/errors.ts
+++ src/common/constants/errors.ts
@@ -2,4 +2,5 @@ export const unknownOptionResponse = (option: string) =>
     `Unknown option *${option}*. Type \`/deploy\` to see the list of available options`;
 
 export const notEnoughOptionsProvided = (optionsCount: number) =>
-    `You've provided ${optionsCount}, while 4 options are required. Type \`/deploy\` to see the list of available options.`;
+    `You've provided ${optionsCount}, while 4 options are required.` +
+    `Type \`/deploy\` to see the list of available options.`;
